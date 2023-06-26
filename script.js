aa9177c77fd5de163427bde2d15458a7a9026e9e
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-06-26 11:05:30 +0200

    Clean up javascript

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// HTML element variables
+// Divs
 const healthBarsDiv = document.querySelector(".health-bars");
-const playerHeartsDiv = document.querySelector(".player-health .hearts")
-const computerHeartsDiv = document.querySelector(".computer-health .hearts")
-const heartsContainers = document.querySelectorAll(".hearts");
-const playerSelectionDiv = document.querySelector(".player-selection")
+const playerHeartsDiv = document.querySelector(".player-health .hearts");
+const computerHeartsDiv = document.querySelector(".computer-health .hearts");
+const heartsContainersDiv = document.querySelectorAll(".hearts");
+const howToPlayDiv = document.querySelector(".how-to-play");
+const playerSelectionDiv = document.querySelector(".player-selection");
 const computerSelectionDiv = document.querySelector(".computer-selection");
 const messageDiv = document.querySelector(".message");
-const howToPlayBtn = document.querySelector(".how-to-play-button");
-const howToPlayDiv = document.querySelector(".how-to-play");
-const newGameBtn = document.querySelector(".new-game-button");
 const fightersContainerDiv = document.querySelector(".fighters-container");
+
+// Buttons
+const newGameBtn = document.querySelector(".new-game-button");
+const howToPlayBtn = document.querySelector(".how-to-play-button");
 const horseBtn = document.querySelector(".horse-button");
 const spearmanBtn = document.querySelector(".spearman-button");
 const archerBtn = document.querySelector(".archer-button");
@@ -20,48 +22,49 @@ const SPEARMAN = "spearman";
 const ARCHER = "archer";
 const STARTING_HEALTH = 5;
 
-// Starting variables
+// Health variables
 let playerHealth;
 let computerHealth;
 
-// Initialize page
+// EventListeners
+newGameBtn.addEventListener('click', newGame);
+howToPlayBtn.addEventListener('click', howToPlay);
 horseBtn.addEventListener("click", playRound);
 spearmanBtn.addEventListener("click", playRound);
 archerBtn.addEventListener("click", playRound);
-newGameBtn.addEventListener('click', newGame);
-howToPlayBtn.addEventListener('click', howToPlay);
 
-function newGame() {
-	healthBarsDiv.style.visibility = "visible";
-	fightersContainerDiv.style.visibility = "visible";
-	messageDiv.style.display = "block";
-	howToPlayBtn.style.display = "none";
-	newGameBtn.style.display = "none";
-	howToPlayDiv.style.display = "none";
-	messageDiv.innerHTML = `<div class="txt1">Choose your fighter!</div>`;
-	playerSelectionDiv.innerHTML = "";
-	computerSelectionDiv.innerHTML = "";
-	playerHealth = STARTING_HEALTH;
-	computerHealth = STARTING_HEALTH;
-	setHearts(STARTING_HEALTH);
+// CSS Helper functions
+function clearInnerHTML(arrayOfElements) {
+	arrayOfElements.forEach((element) => {
+		element.innerHTML = "";
+	});
 }
 
-function howToPlay() {
-	messageDiv.style.display = "none";
-	howToPlayBtn.style.display = "none";
-	newGameBtn.style.display = "flex";
-	healthBarsDiv.style.visibility = "hidden";
-	howToPlayDiv.style.display = "flex";
-	playerSelectionDiv.innerHTML = "";
-	computerSelectionDiv.innerHTML = "";
+function setVisible(arrayOfElements) {
+	arrayOfElements.forEach((element) => {
+		element.style.visibility = "visible";
+	});
 }
 
-function gameOver() {
-	fightersContainerDiv.style.visibility = "hidden";
-	newGameBtn.style.display = "flex";
-	howToPlayBtn.style.display = "flex";
+function setHidden(arrayOfElements) {
+	arrayOfElements.forEach((element) => {
+		element.style.visibility = "hidden";
+	});
 }
 
+function setDisplayNone(arrayOfElements) {
+	arrayOfElements.forEach((element) => {
+		element.style.display = "none";
+	});
+}
+
+function setDisplayFlex(arrayOfElements) {
+	arrayOfElements.forEach((element) => {
+		element.style.display = "flex";
+	});
+}
+
+// Game helper functions
 function randInt(number) {
 	return Math.floor(Math.random() * number);
 }
@@ -71,12 +74,56 @@ function getComputerSelection() {
 	return choices[randInt(choices.length)];
 }
 
-function playRound() {
-	const playerSelection = this.value;
-	playerSelectionDiv.innerHTML = `<img src="img/${playerSelection}.png">`;
+function setHearts(n) {
+	heartsContainersDiv.forEach((heartsContainer) => {
+		clearInnerHTML(heartsContainer);
+		for (let i = 0; i < STARTING_HEALTH; i++) {
+			const HEART_ICON = document.createElement("img");
+			HEART_ICON.src = "img/heart.png";
+			HEART_ICON.width = "30";
+			heartsContainer.appendChild(HEART_ICON);
+		};
+	});
+}
 
+// Main functions
+function newGame() {
+	clearInnerHTML([playerSelectionDiv, computerSelectionDiv]);
+	setDisplayNone([newGameBtn, howToPlayBtn, howToPlayDiv]);
+	setDisplayFlex([messageDiv]);
+	setHearts(STARTING_HEALTH);
+	setVisible([healthBarsDiv, fightersContainerDiv]);
+	playerHealth = STARTING_HEALTH;
+	computerHealth = STARTING_HEALTH;
+	messageDiv.innerHTML = `<div class="txt1">Choose your fighter!</div>`;
+}
+
+function howToPlay() {
+	clearInnerHTML([playerSelectionDiv, computerSelectionDiv]);
+	setDisplayNone([messageDiv, howToPlayBtn]);
+	setDisplayFlex([newGameBtn, howToPlayDiv]);
+	setHidden([healthBarsDiv]);
+}
+
+function gameOver(playerHealth, computerHealth) {
+	setDisplayFlex([newGameBtn, howToPlayBtn]);
+	setHidden([fightersContainerDiv]);
+
+	if (playerHealth > computerHealth) {
+		messageDiv.innerHTML = `<div class="txt1">YOU ARE VICTORIOUS!</div>`;
+	} else {
+		messageDiv.innerHTML = `<div class="txt1">YOU HAVE BEEN DEFEATED!</div>`;
+	}
+}
+
+function playRound() {
+
+	const playerSelection = this.value;
 	const computerSelection = getComputerSelection();
+
+	playerSelectionDiv.innerHTML = `<img src="img/${playerSelection}.png">`;
 	computerSelectionDiv.innerHTML = `<img src="img/${computerSelection}.png">`;
+
 
 	if (playerSelection === computerSelection) {
 		messageDiv.innerHTML =
@@ -90,7 +137,7 @@ function playRound() {
 		playerSelection === ARCHER && computerSelection === HORSE
 	) {
 		messageDiv.innerHTML =
-			`<div class="txt1">YOU LOSE</div
+			`<div class="txt1">YOU LOSE</div>
 			<div class="txt2">The enemy's ${computerSelection} beats 
 			your ${playerSelection}</div>`;
 		playerHealth--;
@@ -109,27 +156,8 @@ function playRound() {
 		computerHeartsDiv.removeChild(computerHeartsDiv.firstElementChild);
 	}
 
-	if (playerHealth < 1) {
-		gameOver();
-		messageDiv.innerHTML = `<div class="txt1">YOU HAVE BEEN DEFEATED!</div>`;
-	} else if (computerHealth < 1) {
-		gameOver();
-		messageDiv.innerHTML = `<div class="txt1">YOU ARE VICTORIOUS!</div>`;
+	if (playerHealth < 1 || computerHealth < 1) {
+		gameOver(playerHealth, computerHealth);
 	}
 
 }
-
-
-
-function setHearts(n) {
-	heartsContainers.forEach((heartsContainer) => {
-		heartsContainer.innerHTML = "";
-		for (let i = 0; i < STARTING_HEALTH; i++) {
-			const HEART_ICON = document.createElement("img");
-			HEART_ICON.src = "img/heart.png";
-			HEART_ICON.width = "30";
-			e.appendChild(HEART_ICON);
-		};
-	});
-}
-
